a37a8737fc6422bfb07df733d4b3c534d3bdafbd
aryan14012012 <rayarjun452@gmail.com>
2026-07-13 10:45:06 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,19 @@
+
+
+snack_name="chips"
+price=2.5
+quantity=3
+is_available=True
+
+
+print("Snack Name:", snack_name)
+print("Price:", price)
+print("Quantity:", quantity)
+print("Is Available:", is_available)
+
+
+
+print(type(snack_name))
+print(type(price))
+print(type(quantity))
+print(type(is_available))
