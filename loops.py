e5d4947d419dccfe932f11fd00bbda009654c922
aryan14012012 <rayarjun452@gmail.com>
2026-07-18 21:05:42 +0530

    star

--- loops.py
+++ loops.py
@@ -1,3 +1,14 @@
 for a in range(1,11):
     
     print(f"22  x {a}  = {22 * a}")
+    
+# stars------------------------------------------
+
+n = int(input("Enter the number of rows :"))
+for i in range(1,n+1):
+    for j in range(i):
+      print('*', end='')    
+    print()
+        
+     
+
