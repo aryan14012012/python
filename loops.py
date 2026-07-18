0743d2aea18ae9fed1cee11889a315d4c7853322
aryan14012012 <rayarjun452@gmail.com>
2026-07-18 21:25:27 +0530

    prime number

--- loops.py
+++ loops.py
@@ -22,3 +22,25 @@ while num <=10:
           
 print(f"The sum of the first ten natural number is {total_sum}")
 
+# prime number------------------------
+
+num= int(input("Enter a number:"))
+
+if num > 1:
+     
+     
+     for i in range(2,int(num**0.5)+  1):
+         
+        if num % i ==0:
+            print(f"{num} is not a prime number.")
+            break
+        
+     else:
+    
+         print(f"{num} is a prime number.")
+    
+    
+else:
+
+    print(f"{num} is not a prime number.")
+                     
